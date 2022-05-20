0d21e5d046ea8c3cd42f6ababe37029a779678cc
WSIDGonggang <quixotic1024@gmail.com>
2022-05-20 17:12:25 +0900

    initial commit

--- homepage/views.py
+++ homepage/views.py
@@ -2,12 +2,9 @@ from urllib import request
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 
-
 def homepage(request):
     return render(
         request, 'homepage/main.html', {})
-        
-
 
 def index(request):
     return render(
@@ -26,9 +23,10 @@ def upload1(request):
     if request.method == 'POST':
         upload_file = request.FILES.get('file') # 파일 객체
         name = upload_file.name # 파일 이름
+        
         size = upload_file.size # 파일 크기
     with open(name, 'wb') as file: # 파일 저장
         for chunk in upload_file.chunks():
-        file.write(chunk)
+            file.write(chunk)
         return HttpResponse('%s<br>%s' % (name, size))
     return render(request, 'file/upload1.html')
