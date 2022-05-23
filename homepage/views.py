d3cf38c0ab5828cd28fb100d55d2697968580999
JUYEONCODE <joseph.oh0312@gmail.com>
2022-05-23 10:58:40 +0900

    파일 업로드

--- homepage/views.py
+++ homepage/views.py
@@ -1,6 +1,6 @@
 from urllib import request
 from django.shortcuts import redirect, render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 
 def homepage(request):
@@ -21,14 +21,36 @@ def contact(request):
     return render(
         request, 'homepage/contact.html', {})
         
+def index1(request):
+    return render(
+        request, 'homepage/index1.html', {})
 
-def upload1(request):
+
+def test(request):
     if request.method == 'POST':
         upload_file = request.FILES.get('file') # 파일 객체
         name = upload_file.name # 파일 이름
         size = upload_file.size # 파일 크기
-    with open(name, 'wb') as file: # 파일 저장
-        for chunk in upload_file.chunks():
-        file.write(chunk)
+        with open(name, 'wb') as file: # 파일 저장
+            for chunk in upload_file.chunks():
+                file.write(chunk)
         return HttpResponse('%s<br>%s' % (name, size))
-    return render(request, 'file/upload1.html')
+    return render(request, 'homepage/test.html')
+
+def upload(request):
+    if request.method == 'POST':
+        upload_file = request.FILES.get('files') # 파일 객체
+        name = upload_file.name # 파일 이름
+        size = upload_file.size # 파일 크기
+        print(name, size)
+        with open('static/photo/%s' % name, 'wb') as file: # 파일 저장
+            for chunk in upload_file.chunks():
+                file.write(chunk)
+
+        context = {
+            'data': {
+                'length': 1
+            }
+        }
+
+        return JsonResponse(context)
